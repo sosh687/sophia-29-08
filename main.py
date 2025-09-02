04e474bda97d0b334dfa8e51a623f514f92849ea
Mush <sophiamoritadantas7@gmail.com>
2025-09-02 12:38:43 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,18 +3,20 @@ import pygame
 # Isso inicio e  modulo do Pygame
 pygame.init()
 
+# Definindo o tamanho da janela
 WIDTH, HEIGTH = 800,600
-
 screen = pygame.displayset_mode((WIDTH, HEIGTH))
-pygame.display.set_caption("Menu Joguinho")
+pygame.display.set_caption("Janela Simples")
 
+# Loop principal do jogo
 running = True
-
 while running:
     for event in pygame.get():
         if event.type == pygame.QUIT:
             running = False
 
+    # Atualizar a tela        
     pygame.display.flip() 
 
-pygame.quit
+ # Finalizar o Pygame
+pygame.ouit()
